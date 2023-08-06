7351f5c47e82a271c69d402baae6065fdfc5aa65
Daria <138609150+paketaban0266@users.noreply.github.com>
2023-08-06 16:33:23 +0300

    Update AbletonLinkConfig.cmake

--- AbletonLinkConfig.cmake
+++ AbletonLinkConfig.cmake
@@ -45,3 +45,5 @@ endif()
 include(${CMAKE_CURRENT_LIST_DIR}/cmake_include/AsioStandaloneConfig.cmake)
 set_property(TARGET Ableton::Link APPEND PROPERTY
  INTERFACE_LINK_LIBRARIES
+ AsioStandalone::AsioStandalone
+)
