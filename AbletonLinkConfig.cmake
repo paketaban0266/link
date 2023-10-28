c92351568c094ac54870bc3d3ec5ee187e3ade3c
Daria <138609150+paketaban0266@users.noreply.github.com>
2023-10-28 14:08:38 +0300

    Update AbletonLinkConfig.cmake

--- AbletonLinkConfig.cmake
+++ AbletonLinkConfig.cmake
@@ -17,3 +17,8 @@ if(UNIX)
   set_property(TARGET Ableton::Link APPEND PROPERTY
   INTERFACE_COMPILE_DEFINITIONS
     LINK_PLATFORM_UNIX=1
+)
+endif()
+
+if(APPLE)
+  set_property(TARGET Ableton::Link APPEND PROPERTY
