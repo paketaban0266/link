5e15c202b1a9c51c3bcd73606c5745d8cd17f766
Daria <138609150+paketaban0266@users.noreply.github.com>
2023-11-09 11:32:28 +0200

    Update AbletonLinkConfig.cmake

--- AbletonLinkConfig.cmake
+++ AbletonLinkConfig.cmake
@@ -43,3 +43,5 @@ elseif(CMAKE_SYSTEM_NAME MATCHES "Linux|kFreeBSD|GNU")
 endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/cmake_include/AsioStandaloneConfig.cmake)
+set_property(TARGET Ableton::Link APPEND PROPERTY
+  INTERFACE_LINK_LIBRARIES
