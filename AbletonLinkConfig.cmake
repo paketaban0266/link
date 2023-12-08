138ad9bf78e79330350004f83c0191ab7f59819d
Daria <138609150+paketaban0266@users.noreply.github.com>
2023-12-08 14:17:16 +0200

    Update AbletonLinkConfig.cmake

--- AbletonLinkConfig.cmake
+++ AbletonLinkConfig.cmake
@@ -1,3 +1,6 @@
 if(CMAKE_VERSION VERSION_LESS 3.0)
   message(FATAL_ERROR "CMake 3.0 or greater is required")
 endif()
+
+add_library(Ableton::Link IMPORTED INTERFACE)
+set_property(TARGET Ableton::Link APPEND PROPERTY
